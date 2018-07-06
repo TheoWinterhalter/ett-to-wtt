3de797d61c42d8444b3c09e8d6b42a2471040d94
Théo Winterhalter <isheeft@gmail.com>
2018-07-06 14:40:23 +0200

    Prove substitution lemma for ETT

--- theories/XTypingLemmata.v
+++ theories/XTypingLemmata.v
@@ -263,5 +263,236 @@ Proof.
 
 Defined.
 
-
 End Lift.
+
+Section Subst.
+
+Context `{Sort_notion : Sorts.notion}.
+
+Ltac sh h :=
+  lazymatch goal with
+  | [ type_subst :
+        forall (Σ : sglobal_context) (Γ Δ : scontext) (t A B u : sterm),
+          Σ;;; Γ,, B ,,, Δ |-x t : A ->
+          type_glob Σ ->
+          Σ;;; Γ |-x u : B -> Σ;;; Γ ,,, subst_context u Δ |-x
+          t {#|Δ| := u} : A {#|Δ| := u},
+     cong_subst :
+       forall (Σ : sglobal_context) (Γ Δ : scontext) (t1 t2 A B u : sterm),
+         Σ;;; Γ,, B ,,, Δ |-x t1 = t2 : A ->
+         type_glob Σ ->
+         Σ;;; Γ |-x u : B -> Σ;;; Γ ,,, subst_context u Δ |-x
+         t1 {#|Δ| := u} = t2 {#|Δ| := u} : A {#|Δ| := u}
+    |- _ ] =>
+    lazymatch type of h with
+    | _ ;;; ?Γ' ,, ?B' ,,, ?Δ' |-x _ : ?T' =>
+      eapply meta_conv ; [
+        eapply meta_ctx_conv ; [
+          eapply type_subst with (Γ := Γ') (Δ := Δ') (A := T') ; [
+            exact h
+          | assumption
+          | assumption
+          ]
+        | .. ]
+      | .. ]
+    | _ ;;; (?Γ' ,, ?B' ,,, ?Δ') ,, ?d' |-x _ : ?T' =>
+      eapply meta_conv ; [
+        eapply meta_ctx_conv ; [
+          eapply type_subst with (Γ := Γ') (Δ := Δ' ,, d') (A := T') ; [
+            exact h
+          | assumption
+          | assumption
+          ]
+        | .. ]
+      | .. ]
+    | _ ;;; (?Γ' ,, ?B' ,,, ?Δ') ,, ?d',, ?d'' |-x _ : ?T' =>
+      eapply meta_conv ; [
+        eapply meta_ctx_conv ; [
+          eapply type_subst with (Γ := Γ') (Δ := (Δ' ,, d') ,, d'') (A := T') ; [
+            exact h
+          | assumption
+          | assumption
+          ]
+        | .. ]
+      | .. ]
+    | _ ;;; ?Γ' ,, ?B' ,,, ?Δ' |-x _ = _ : ?T' =>
+      eapply meta_eqconv ; [
+        eapply meta_eqctx_conv ; [
+          eapply cong_subst with (Γ := Γ') (Δ := Δ') (A := T') ; [
+            exact h
+          | assumption
+          | assumption
+          ]
+        | .. ]
+      | .. ]
+    | _ ;;; (?Γ' ,, ?B' ,,, ?Δ') ,, ?d' |-x _ = _ : ?T' =>
+      eapply meta_eqconv ; [
+        eapply meta_eqctx_conv ; [
+          eapply cong_subst with (Γ := Γ') (Δ := Δ' ,, d') (A := T') ; [
+            exact h
+          | assumption
+          | assumption
+          ]
+        | .. ]
+      | .. ]
+    | _ ;;; (?Γ' ,, ?B' ,,, ?Δ') ,, ?d',, ?d'' |-x _ = _ : ?T' =>
+      eapply meta_eqconv ; [
+        eapply meta_eqctx_conv ; [
+          eapply cong_subst with (Γ := Γ') (Δ := (Δ' ,, d') ,, d'') (A := T') ; [
+            exact h
+          | assumption
+          | assumption
+          ]
+        | .. ]
+      | .. ]
+    end ; try (cbn ; reflexivity)
+  | _ => fail "cannot find type_subst, cong_subst"
+  end.
+
+Ltac esh :=
+  lazymatch goal with
+  | h : _ ;;; _ |-x ?t : _ |- _ ;;; _ |-x ?t{ _ := _ } : _ => sh h
+  | h : _ ;;; _ |-x ?t = _ : _ |- _ ;;; _ |-x ?t{ _ := _ } = _ : _ =>
+    sh h
+  | _ => fail "not handled by esh"
+  end.
+
+Fixpoint type_subst {Σ Γ Δ t A B u}
+  (h : Σ ;;; Γ ,, B ,,, Δ |-x t : A) {struct h} :
+  type_glob Σ ->
+  Σ ;;; Γ |-x u : B ->
+  Σ ;;; Γ ,,, subst_context u Δ |-x t{ #|Δ| := u } : A{ #|Δ| := u }
+
+with cong_subst {Σ Γ Δ t1 t2 A B u}
+  (h : Σ ;;; Γ ,, B ,,, Δ |-x t1 = t2 : A) {struct h} :
+  type_glob Σ ->
+  Σ ;;; Γ |-x u : B ->
+  Σ ;;; Γ ,,, subst_context u Δ |-x t1{ #|Δ| := u }
+  = t2{ #|Δ| := u } : A{ #|Δ| := u }
+
+with wf_subst {Σ Γ Δ B u}
+  (h : wf Σ (Γ ,, B ,,, Δ)) {struct h} :
+  type_glob Σ ->
+  Σ ;;; Γ |-x u : B ->
+  wf Σ (Γ ,,, subst_context u Δ)
+.
+Proof.
+  (* type_subst *)
+  - { intros hg hu.
+      dependent destruction h.
+      - cbn. case_eq (#|Δ| ?= n) ; intro e ; bprop e.
+        + assert (h : n >= #|Δ|) by omega.
+          rewrite safe_nth_ge' with (h0 := h).
+          assert (n - #|Δ| = 0) by omega.
+          set (ge := ge_sub isdecl h).
+          generalize ge.
+          rewrite H. intro ge'.
+          cbn. rewrite substP3 by omega.
+          subst.
+          replace #|Δ| with #|subst_context u Δ|
+            by (now rewrite subst_context_length).
+          eapply @type_lift with (Ξ := []) (Δ := subst_context u Δ).
+          * cbn. assumption.
+          * assumption.
+          * eapply wf_subst ; eassumption.
+        + assert (h : n >= #|Δ|) by omega.
+          rewrite safe_nth_ge' with (h0 := h).
+          set (ge := ge_sub isdecl h).
+          destruct n ; try easy.
+          rewrite substP3 by omega.
+          generalize ge.
+          replace (S n - #|Δ|) with (S (n - #|Δ|)) by omega.
+          cbn. intro ge'.
+          eapply meta_conv.
+          * eapply type_Rel. eapply wf_subst ; eassumption.
+          * erewrite safe_nth_ge'.
+            f_equal. eapply safe_nth_cong_irr.
+            rewrite subst_context_length. reflexivity.
+        + assert (h : n < #|Δ|) by omega.
+          rewrite @safe_nth_lt with (isdecl' := h).
+          match goal with
+          | |- _ ;;; _ |-x _ : ?t{?d := ?u} =>
+            replace (subst u d t) with (t{((S n) + (#|Δ| - (S n)))%nat := u})
+              by (f_equal ; omega)
+          end.
+          rewrite substP2 by omega.
+          eapply meta_conv.
+          * eapply type_Rel.
+            eapply wf_subst ; eassumption.
+          * f_equal.
+            erewrite safe_nth_lt.
+            eapply safe_nth_subst_context.
+      - cbn. apply type_Sort. eapply wf_subst ; eassumption.
+      - cbn. eapply type_Prod ; esh.
+      - cbn. eapply type_Lambda ; esh.
+      - cbn.
+        change ((B0 {0 := u0}) {#|Δ| := u})
+          with ((B0 {0 := u0}) {0 + #|Δ| := u}).
+        rewrite substP4. cbn.
+        eapply type_App ; esh.
+      - cbn. eapply type_Sum ; esh.
+      - cbn. eapply type_Pair ; esh.
+        change (#|Δ|) with (0 + #|Δ|)%nat.
+        rewrite substP4. reflexivity.
+      - cbn. eapply type_Pi1 ; esh.
+      - cbn.
+        change (#|Δ|) with (0 + #|Δ|)%nat.
+        rewrite substP4. cbn.
+        eapply type_Pi2 ; esh.
+      - cbn. eapply type_Eq ; esh.
+      - cbn. eapply type_Refl ; esh.
+      - cbn. erewrite subst_ax_type by eassumption.
+        eapply type_Ax.
+        + now eapply wf_subst.
+        + assumption.
+      - cbn. eapply type_conv ; esh.
+    }
+
+  (* cong_subst *)
+  - { intros hg hu.
+      dependent destruction h.
+      - constructor. esh.
+      - constructor. esh.
+      - eapply eq_transitivity ; esh.
+      - cbn.
+        change #|Δ| with (0 + #|Δ|)%nat.
+        rewrite 2!substP4. cbn.
+        eapply eq_beta ; esh.
+      - eapply eq_conv ; esh.
+      - cbn. eapply cong_Prod ; esh.
+      - cbn. eapply cong_Lambda ; esh.
+      - cbn. change #|Δ| with (0 + #|Δ|)%nat.
+        rewrite substP4. cbn.
+        eapply cong_App ; esh.
+      - cbn. eapply cong_Sum ; esh.
+      - cbn. eapply cong_Pair ; esh.
+        + change (#|Δ|) with (0 + #|Δ|)%nat.
+          rewrite substP4. reflexivity.
+        + change (#|Δ|) with (0 + #|Δ|)%nat.
+          rewrite substP4. reflexivity.
+        + change (#|Δ|) with (0 + #|Δ|)%nat.
+          rewrite substP4. reflexivity.
+      - cbn. eapply cong_Pi1 ; esh.
+      - cbn. 
+        change (#|Δ|) with (0 + #|Δ|)%nat.
+        rewrite substP4. cbn.
+        eapply cong_Pi2 ; esh.
+      - cbn. eapply cong_Eq ; esh.
+      - cbn. eapply cong_Refl ; esh.
+      - eapply reflection with (e0 := e{#|Δ| := u}). esh.
+    }
+
+  (* wf_subst *)
+  - { intros hg hu.
+      destruct Δ.
+      - cbn. dependent destruction h. assumption.
+      - dependent destruction h. cbn. econstructor.
+        + eapply wf_subst ; eassumption.
+        + esh.
+    }
+
+  Unshelve.
+  all: try rewrite !length_cat ; try rewrite !subst_context_length ; omega.
+Defined.
+
+End Subst.
