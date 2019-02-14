a6dc461115e5e0baa5695014a613b800648ec75a
Théo Winterhalter <isheeft@gmail.com>
2019-02-14 14:11:49 +0100

    Complete wttinfer expect Σ

--- theories/WChecker.v
+++ theories/WChecker.v
@@ -41,6 +41,12 @@ Definition getheq (T : wterm) : option (wterm * wterm * wterm * wterm) :=
   | _ => None
   end.
 
+Definition getpack (T : wterm) : option (wterm * wterm) :=
+  match T with
+  | wPack A1 A2 => ret (A1,A2)
+  | _ => None
+  end.
+
 Definition assert_true (b : bool) : option unit :=
   if b then ret tt else None.
 
@@ -50,63 +56,65 @@ Definition assert_eq (u v : wterm) :=
 Definition assert_eq_sort (s1 s2 : sort) : option unit :=
   if Sorts.eq_dec s1 s2 then ret tt else None.
 
-Fixpoint _wttinfer (Σ : wglobal_context) (Γ : wcontext) (t : wterm)
+Fixpoint wttinfer (Σ : wglobal_context) (Γ : wcontext) (t : wterm)
   : option wterm :=
   match t with
-  | wRel n => nth_error Γ n
-  | wSort s => ret (wSort (succ s))
+  | wRel n =>
+    nth_error Γ n
+  | wSort s =>
+    ret (wSort (succ s))
   | wProd n A B =>
-    s1 <- getsort =<< _wttinfer Σ Γ A ;;
-    s2 <- getsort =<< _wttinfer Σ (Γ ,, A) B ;;
+    s1 <- getsort =<< wttinfer Σ Γ A ;;
+    s2 <- getsort =<< wttinfer Σ (Γ ,, A) B ;;
     ret (wSort (prod_sort s1 s2))
   | wLambda n A t =>
-    (* _ <- getsort =<< _wttinfer Σ Γ A ;; *)
-    B <- _wttinfer Σ (Γ ,, A) t ;;
+    (* _ <- getsort =<< wttinfer Σ Γ A ;; *)
+    B <- wttinfer Σ (Γ ,, A) t ;;
     ret (wProd n A B)
   | wApp u v =>
-    Π <- getprod =<< _wttinfer Σ Γ u ;;
+    Π <- getprod =<< wttinfer Σ Γ u ;;
     let '(A,B) := Π in
-    assert_eq A =<< _wttinfer Σ Γ v ;;
+    assert_eq A =<< wttinfer Σ Γ v ;;
     ret (B{ 0 := u })
   (* TODO Σ *)
   | wEq A u v =>
-    s <- getsort =<< _wttinfer Σ Γ A ;;
-    assert_eq A =<< _wttinfer Σ Γ u ;;
-    assert_eq A =<< _wttinfer Σ Γ v ;;
+    s <- getsort =<< wttinfer Σ Γ A ;;
+    assert_eq A =<< wttinfer Σ Γ u ;;
+    assert_eq A =<< wttinfer Σ Γ v ;;
     ret (wSort (eq_sort s))
   | wRefl A u =>
-    getsort =<< _wttinfer Σ Γ A ;;
-    assert_eq A =<< _wttinfer Σ Γ u ;;
+    getsort =<< wttinfer Σ Γ A ;;
+    assert_eq A =<< wttinfer Σ Γ u ;;
     ret (wEq A u u)
   | wJ A u P w v p =>
-    getsort =<< _wttinfer Σ Γ A ;;
-    assert_eq A =<< _wttinfer Σ Γ u ;;
-    assert_eq A =<< _wttinfer Σ Γ v ;;
-    getsort =<< _wttinfer Σ (Γ ,, A ,, (wEq (lift0 1 A) (lift0 1 u) (wRel 0))) P ;;
-    assert_eq (wEq A u v) =<< _wttinfer Σ Γ p ;;
-    assert_eq (P{ 1 := u }{ 0 := wRefl A u }) =<< _wttinfer Σ Γ w ;;
+    getsort =<< wttinfer Σ Γ A ;;
+    assert_eq A =<< wttinfer Σ Γ u ;;
+    assert_eq A =<< wttinfer Σ Γ v ;;
+    getsort =<< wttinfer Σ (Γ ,, A ,, (wEq (lift0 1 A) (lift0 1 u) (wRel 0))) P ;;
+    assert_eq (wEq A u v) =<< wttinfer Σ Γ p ;;
+    assert_eq (P{ 1 := u }{ 0 := wRefl A u }) =<< wttinfer Σ Γ w ;;
     ret (P{ 1 := v }{ 0 := p })
   | wTransport A B p t =>
-    s <- getsort =<< _wttinfer Σ Γ A ;;
-    assert_eq_sort s =<< getsort =<< _wttinfer Σ Γ B ;;
-    assert_eq (wEq (wSort s) A B) =<< _wttinfer Σ Γ p ;;
-    assert_eq A =<< _wttinfer Σ Γ t ;;
+    s <- getsort =<< wttinfer Σ Γ A ;;
+    assert_eq_sort s =<< getsort =<< wttinfer Σ Γ B ;;
+    assert_eq (wEq (wSort s) A B) =<< wttinfer Σ Γ p ;;
+    assert_eq A =<< wttinfer Σ Γ t ;;
     ret B
   | wBeta t u =>
-    A <- _wttinfer Σ Γ u ;;
-    B <- _wttinfer Σ (Γ,, A) t ;;
+    A <- wttinfer Σ Γ u ;;
+    B <- wttinfer Σ (Γ,, A) t ;;
     ret (wEq (B{ 0 := u }) (wApp (wLambda nAnon A t) u) (t{ 0 := u }))
   | wHeq A a B b =>
-    s <- getsort =<< _wttinfer Σ Γ A ;;
-    assert_eq_sort s =<< getsort =<< _wttinfer Σ Γ B ;;
-    assert_eq A =<< _wttinfer Σ Γ a ;;
-    assert_eq B =<< _wttinfer Σ Γ b ;;
+    s <- getsort =<< wttinfer Σ Γ A ;;
+    assert_eq_sort s =<< getsort =<< wttinfer Σ Γ B ;;
+    assert_eq A =<< wttinfer Σ Γ a ;;
+    assert_eq B =<< wttinfer Σ Γ b ;;
     ret (wSort s)
   | wHeqPair p q =>
-    E <- geteq =<< _wttinfer Σ Γ p ;;
+    E <- geteq =<< wttinfer Σ Γ p ;;
     let '(T,A,B) := E in
     s <- getsort T ;;
-    E' <- geteq =<< _wttinfer Σ Γ q ;;
+    E' <- geteq =<< wttinfer Σ Γ q ;;
     let '(B',ta,b) := E' in
     assert_eq B B' ;;
     ti <- gettransport ta ;;
@@ -115,13 +123,35 @@ Fixpoint _wttinfer (Σ : wglobal_context) (Γ : wcontext) (t : wterm)
     assert_eq B B' ;;
     ret (wHeq A a B b)
   | wHeqTy A B p =>
-    H <- getheq =<< _wttinfer Σ Γ p ;;
+    H <- getheq =<< wttinfer Σ Γ p ;;
     let '(A',a,B',b) := H in
     assert_eq A A' ;;
     assert_eq B B' ;;
-    s <- getsort =<< _wttinfer Σ Γ A ;;
-    assert_eq_sort s =<< getsort =<< _wttinfer Σ Γ A ;;
+    s <- getsort =<< wttinfer Σ Γ A ;;
+    assert_eq_sort s =<< getsort =<< wttinfer Σ Γ A ;;
     ret (wEq (wSort s) A B)
+  | wHeqTm p =>
+    H <- getheq =<< wttinfer Σ Γ p ;;
+    let '(A,a,B,b) := H in
+    ret (wEq B (wTransport A B (wHeqTy A B p) a) b)
+  | wPack A1 A2 =>
+    s <- getsort =<< wttinfer Σ Γ A1 ;;
+    assert_eq_sort s =<< getsort =<< wttinfer Σ Γ A2 ;;
+    ret (wSort s)
+  | wProjT1 p =>
+    P <- getpack =<< wttinfer Σ Γ p ;;
+    let '(A1,A2) := P in
+    ret A1
+  | wProjT2 p =>
+    P <- getpack =<< wttinfer Σ Γ p ;;
+    let '(A1,A2) := P in
+    ret A2
+  | wProjTe p =>
+    P <- getpack =<< wttinfer Σ Γ p ;;
+    let '(A1,A2) := P in
+    ret (wHeq A1 (wProjT1 p) A2 (wProjT2 p))
+  | wAx id =>
+    lookup_glob Σ id
   | _ => None
   end.
 
